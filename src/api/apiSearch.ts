4b4b92ee537a6311eda2888c5e76fe46d73fffe5
mperalta <mperalta@gmail.com>
2024-05-12 16:15:57 -0400

    add api config

--- src/api/apiSearch.ts
+++ src/api/apiSearch.ts
@@ -1,4 +1,4 @@
-import {AppConfig} from '../config/appConfig';
+import {Config} from '../../env';
 import {searchMovieDto} from '../models/dto/search.dto';
 import {SearchResponse} from '../models/responses/searchResponse';
 import API from './api';
@@ -6,7 +6,7 @@ import API from './api';
 export const searchMovie = async ({title, year}: searchMovieDto) => {
   try {
     const res = await API.get<SearchResponse>(
-      `?apikey=${AppConfig.apiKey}&t=${title}&y=${year}`,
+      `?apikey=${Config.apiKey}&t=${title}&y=${year}`,
     );
     if (res.status === 200) {
       return res.data;
---
+++ src/config/appConfig.ts
@@ -0,0 +1,3 @@
+export const AppConfig = {
+  baseUrl: 'https://www.omdbapi.com/',
+};
