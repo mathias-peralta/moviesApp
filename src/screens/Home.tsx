a5e2897bb1f2540d66a3c45e3c5ec899f0d6b681
mperalta <mperalta@gmail.com>
2024-05-12 14:23:53 -0400

    configure rn paper

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const HomeScreen = () => {
   return (
     <View>
+      <AntDesign name="stepforward" size={32} color="black" />
       <Text>HomeScreen</Text>
     </View>
   );
